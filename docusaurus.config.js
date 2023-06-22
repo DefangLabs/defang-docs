7cf3c79dfe71c53025dfc8a5994a733473a93646
Cynthia <136117477+Cyyncy@users.noreply.github.com>
2023-06-22 11:47:23 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -129,7 +129,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Defang All Rights Reserved.`,
       },
       prism: {
         theme: lightCodeTheme,
