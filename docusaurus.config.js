5a9be5a4fa630e53f5d95343172b4279799f3dca
Cynthia <136117477+Cyyncy@users.noreply.github.com>
2023-06-28 12:51:10 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -138,4 +138,26 @@ const config = {
   },
 };
 
+// Check if routes array already exists
+const existingRoutes = config.themeConfig.routes;
+
+if (!existingRoutes) {
+  // If routes array does not exist, add it
+  config.themeConfig.routes = [
+    {
+      path: '/docs/',
+      exact: true,
+      component: '@site/static/redirect.html',
+    },
+  ];
+} else {
+  // If routes array already exists, append the redirect route to it
+  config.themeConfig.routes.push({
+    path: '/docs/',
+    exact: true,
+    component: '@site/static/redirect.html',
+  });
+}
+
 module.exports = config;
+
