a44bacee022c6062956a0e7fcdbfe3eb9caa4f4e
Cynthia <136117477+Cyyncy@users.noreply.github.com>
2023-06-29 00:18:09 -0700

    Create docs.js

---
+++ src/pages/docs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function DocsPage() {
+  // Perform any additional logic or rendering you need for this page
+  // You can add links or buttons to navigate to `https://docs.defang.io/docs`
+  return (
+    <div>
+      <h1>Documentation</h1>
+      <p>
+        Click <a href="https://docs.defang.io/docs">here</a> to access the documentation.
+      </p>
+    </div>
+  );
+}
+
+export default DocsPage;
