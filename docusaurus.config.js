bc034822a188c28af8dc138a2e43a25b05d81b22
Cynthia <136117477+Cyyncy@users.noreply.github.com>
2023-07-12 16:06:17 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,13 +39,7 @@ const config = {
       {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
+          editUrl: 'https://github.com/defang-io/defang-docs/edit/master/docs/{{docPath}}',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -139,4 +133,5 @@ const config = {
 };
 
 module.exports = config;
+
 
