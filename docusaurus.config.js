c42d6e359368a35dead14dcdfec791502f55cc3a
Cynthia <136117477+Cyyncy@users.noreply.github.com>
2023-07-12 16:38:49 -0700

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           editUrl:
-            'https://github.com/defang-io/defang-docs/tree/main/docs/{{docPath}}',
+            'https://github.com/defang-io/defang-docs/tree/main/docs',
         },
         blog: {
           showReadingTime: true,
