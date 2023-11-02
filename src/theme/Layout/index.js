fd4461283384043ccf35cbffcbc2b2dcbc51a501
Raphael Titsworth-Morin <raphael@raphaeltm.com>
2023-11-01 19:36:26 -0700

    add analytics properly, including page tracking through docusaurus router and on search queries

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,7 +1,29 @@
-import React, { useEffect } from 'react';
-import Layout from '@theme-original/Layout';
+import { useLocation } from "@docusaurus/router";
+import Layout from "@theme-original/Layout";
+import React, { useEffect } from "react";
+
+let searchTrackingTimeout;
 
 export default function LayoutWrapper(props) {
+  const location = useLocation();
+  useEffect(() => {
+    window?.analytics?.page();
+  }, [location]);
+
+  useEffect(() => {
+    const search = window.document.getElementById("search_input_react");
+    search.onchange = (e) => {
+      if (searchTrackingTimeout) {
+        clearTimeout(searchTrackingTimeout);
+      }
+      searchTrackingTimeout = setTimeout(() => {
+        window?.analytics?.track("docsSearch", {
+          searchQuery: e.target.value,
+        });
+      }, 1000);
+    }
+  }, []);
+
   return (
     <>
       <Layout {...props} />
