baf821d8dfa623ffe09c8c9874408406db2f29f7
Jordan Stephens <jordan@stephens.io>
2024-08-08 14:34:43 -0700

    checkout defang and samples in sibling directories
    to mirror the configuration in our development environments

--- scripts/prebuild.sh
+++ scripts/prebuild.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cd defang/src/cmd/gendocs && go run main.go ../../../defang-docs/docs/cli
+cd ../defang/src/cmd/gendocs && go run main.go ../../../../defang-docs/docs/cli
 cd -
 node scripts/prep-cli-docs.js
 node scripts/prep-samples.js
